2f419b5b2cf7e82d9fc0561e695d73cd8b5b98d9
ozcer <iamreallyoscar@hotmail.com>
2018-02-17 14:47:52 -0800

    green train

--- game_objects.py
+++ game_objects.py
@@ -1,4 +1,5 @@
 import pygame
+from const import *
 
 
 class GameObject:
@@ -11,7 +12,7 @@ class Track(GameObject):
         self.breakpoints = breakpoints
         self.start = start_station
         self.end = end_station
-        self.color = (0, 0, 0)
+        self.color = BLACK
         self.surface = surface
     
     def tick(self):
@@ -25,22 +26,47 @@ class Track(GameObject):
         point_list.append((self.end.x, self.end.y))
         pygame.draw.lines(self.surface, self.color, False, point_list)
 
+
 class Station(GameObject):
     
     def __init__(self, pos: (int, int), surface):
-        self.tracks = []
+        self.tracks = {} # "routeName": Track
         self.x = pos[0]
         self.y = pos[1]
-        self.stationSize = 10
+        self.stationSize = 20
         self.surface = surface
-        print("created station obj")
     
     def addTrack(self, track):
         self.tracks.append(track)
+    
+    def send(self, train, track):
+        pass
     
     def draw(self):
         pygame.draw.rect(self.surface, (0, 0, 255), [self.x - (self.stationSize/2), self.y-(self.stationSize/2), self.stationSize, self.stationSize])
 
     
+    def tick(self):
+        pass
+    
+
+class Train(GameObject):
+    
+    def __init__(self, parked_station, route, surface):
+        self.track = None
+        self.speed = 5
+        self.x, self.y = parked_station.x, parked_station.y
+        self.sprite = pygame.Rect(self.x, self.y, 50, 10)
+        self.surface = surface
+        self.color = GREEN
+
+    def travel_track(self, track):
+
+        for bp in track:
+            pass
+    
+    def draw(self):
+        pygame.draw.rect(self.surface, self.color, self.sprite)
+        
     def tick(self):
         pass
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pygame.locals
 
 pygame.init()
 
-WIDTH = 350
-HEIGHT = 400
+WIDTH = 1250
+HEIGHT = 500
 MAINSURF = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption('Hello World!')
@@ -24,8 +24,11 @@ ctrl.entities.append(sta1)
 sta2 = Station((250,250), MAINSURF)
 ctrl.entities.append(sta2)
 
-t1 = Track(sta1, sta2, [(250, 50), (150,150)], MAINSURF)
-ctrl.entities.append(t1)
+trk1 = Track(sta1, sta2, [(250, 50), (150,150)], MAINSURF)
+ctrl.entities.append(trk1)
+
+trn1 = Train(sta1, "my_route", MAINSURF)
+ctrl.entities.append(trn1)
 while True:  # main game loop
     MAINSURF.fill(WHITE)
     
