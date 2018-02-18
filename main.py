286b85b7cc59af8b701b1659ba1ed13c5baa6c21
Anas Elmi <elmi.anas@gmail.com>
2018-02-17 23:08:46 -0800

    Revert "now we got the zelda that gunna become thomas soon and we got that choo choo shit press mouseleft to give her a whirl ;)"
    This reverts commit 7303afff24caf4ca19148fb4e28a444abfb2e870.

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from game_objects import *
 from pygame.locals import *
 
 pygame.init()
+pygame.mixer.init()
+pygame.mixer.set_num_channels(64)
+pygame.mixer.music.load("zelda.mp3")
+choo_sound = pygame.mixer.Sound("choo.wav")
 
 MAINSURF = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
 DISPLAY_WIDTH, DISPLAY_HEIGHT = MAINSURF.get_size()
@@ -59,6 +63,7 @@ selected_stations = []
 def create_station(pos, surface):
     new_station = Station(pos, surface)
     ctrl.entities.append(new_station)
+    choo_sound.play()
 
 
 def create_track(start, end, breakpoints, surface=MAINSURF):
